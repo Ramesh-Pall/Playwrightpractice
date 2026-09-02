c99e8fd4cf7366e8713159bc56954ba944e70b9b
Ramesh-Pall <ramesh.pallmkuppam@gmail.com>
2026-09-02 20:16:05 +0530

    jenkins file added6

--- playwright.config.js
+++ playwright.config.js
@@ -36,14 +36,14 @@ module.exports = defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
     // {
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
